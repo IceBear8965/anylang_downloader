6a87f85bf9329e0618d4c0fa5e646986392ad5e9
IceBear8965 <icebee088@gmail.com>
2025-07-01 21:00:56 +0200

    Multithreading and loging

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
-from tempfile import NamedTemporaryFile
 from PySide6.QtWidgets import QMainWindow, QApplication
-from PySide6.QtCore import QStandardPaths, QThread, Signal
+from PySide6.QtCore import QStandardPaths, QObject, QThread, Signal
 import sys
 
 from ui.UI_MainWindow import Ui_MainWindow
 from pdf_writer import PDFWriter
 
-class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self):
+class Worker(QObject):
+    finished = Signal()
+    message = Signal(str)
+
+    def __init__(self, url):
         super().__init__()
-        self.setupUi(self)
+        self.url = url
         self.documents_path = QStandardPaths.writableLocation(QStandardPaths.StandardLocation.DocumentsLocation)
         requests.packages.urllib3.disable_warnings()
 
-        self.connect_signals_to_slots()
+    def run(self):
+        self.fetch_book(self.url)
 
-    def fetch_book(self):
-        url = self.urlField.text()
-        if len(url) >= 10:
-            html = requests.get(url).text
-            self.urlField.clear()
-            print("fetched")
-            self.write_book(html)
+    def fetch_book(self, url: str):
+        html = requests.get(url).text
+        self.message.emit("Page downloaded")
+        self.write_book(html)
 
     def write_book(self, html):
         soup = BeautifulSoup(html, "lxml")
@@ -43,7 +43,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 elif not self.check_empty_paragraph(child):
                     try:
                         if child.get("style") == "text-align: center;":
-                           pdf.add_title(child.text)
+                            pdf.add_title(child.text)
                         elif child.get("class") is not None:
                             if child.get("class")[0] == "toc_h":
                                 pdf.add_chapter_title(child.text)
@@ -51,15 +51,54 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                             pdf.add_text(child.text)
                     except Exception as e:
                         print(e)
+            self.message.emit(f"Writed Page {pages.index(page)+1} ot of {len(pages)}")
 
         pdf.output(f"{self.documents_path}/{file_name}")
-        print("done")
+        self.message.emit("File Written")
+        self.finished.emit()
 
     def check_empty_paragraph(self, paragraph):
         return paragraph.text.strip() == ""
 
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.logs = []
+
+        self.connect_signals_to_slots()
+
+    def run_thread(self):
+        url = self.urlField.text()
+        if len(url) >= 10:
+            # Создаём поток и рабочий объект
+            self.thread = QThread()
+            self.worker = Worker(url)
+            self.worker.moveToThread(self.thread)
+
+            # Подключаем сигналы и слоты
+            self.thread.started.connect(self.worker.run)
+            self.worker.message.connect(self.handle_thread_messages)
+            self.worker.finished.connect(self.thread_finished)
+            self.worker.finished.connect(self.thread.quit)
+            self.worker.finished.connect(self.worker.deleteLater)
+            self.thread.finished.connect(self.thread.deleteLater)
+
+            self.thread.start()
+        else:
+            self.logs = []
+            self.logsField.setPlainText("Enter URL")
+
+    def thread_finished(self):
+        self.urlField.clear()
+
+    def handle_thread_messages(self, message):
+        self.logs.append(message)
+        self.logsField.setPlainText("\n".join(self.logs))
+        self.logsField.verticalScrollBar().setValue(self.logsField.verticalScrollBar().maximum())
+
     def connect_signals_to_slots(self):
-        self.runBtn.clicked.connect(self.fetch_book)
+        self.runBtn.clicked.connect(self.run_thread)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
